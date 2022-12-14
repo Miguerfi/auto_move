013f417083569f9d7d9b7e30602bf788ccd174e5
Miguerfi <miguelpintojose00@gmail.com>
2022-12-14 09:06:15 -0300

    now the soruce and the destin folder are defined by the user

--- auto_move.py
+++ auto_move.py
@@ -5,20 +5,19 @@ from pathlib import Path
 mover = 1
 original = 2
 
-rMO = int(input("Deseja Mover(1) ou Restaurar(2)?"))
+
+rMO = int(input("Move(1) or Restore(2)?"))
+source = str(input("Archive Path Location:"))
+dest = str(input("Destin Path:"))
+        
+
 if rMO == 1:
-    
-    source = ""
-    dest = ""
-    
     shutil.move(source, dest)
     print(f"moved {source} to {dest}")
     
     oslist = os.listdir(dest)
     print(f"{oslist}")
-
 if rMO == 2:
-    source = ""
-    dest = ""
     shutil.move(source, dest)
     print(f"{source} restored to {dest}")
+

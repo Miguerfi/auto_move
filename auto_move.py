3078ef359428846e5a05b9a387f257e70a9ac0b5
Miguerfi <miguelpintojose00@gmail.com>
2022-12-14 08:50:48 -0300

    auto move files .py

--- auto_move.py
+++ auto_move.py
@@ -8,30 +8,17 @@ original = 2
 rMO = int(input("Deseja Mover(1) ou Restaurar(2)?"))
 if rMO == 1:
     
-    source = "dev_settings.py"
-    dest = "mwdjango/"
+    source = ""
+    dest = ""
     
     shutil.move(source, dest)
-    print("dev_settings.py movida para mwdjango")
+    print(f"moved {source} to {dest}")
     
     oslist = os.listdir(dest)
     print(f"{oslist}")
 
-    cors = """# CORS_ALLOWED_ORIGINS = [
-#    'http://localhost:3000',
-# ]"""
-    rp = open('mwdjango/dev_settings.py')
-    lines = rp.readlines()
-    for row in lines:
-        if row.find(cors):
-            rep = cors.replace("""# CORS_ALLOWED_ORIGINS = [
-#    'http://localhost:3000',
-# ]""",""" CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
- ]""")
-
 if rMO == 2:
-    source = "mwdjango/dev_settings.py"
-    dest = "dev_settings.py"
+    source = ""
+    dest = ""
     shutil.move(source, dest)
-    print("dev_settings.py foi restaurada para seu local de origem.")
+    print(f"{source} restored to {dest}")
